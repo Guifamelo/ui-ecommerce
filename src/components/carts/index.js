2340d6a5c242bba364d92eb58770014d7587300c
Guifamelo <guilhermefdemelo@hotmail.com>
2023-05-11 00:27:52 -0300

    home&carts css

--- src/components/carts/index.js
+++ src/components/carts/index.js
@@ -1,9 +1,61 @@
 import React from "react";
+import './carts.css'
+import { Link } from "react-router-dom";
 
 const Carts = () => {
     return (
-        <div>
-            <h1>Carts component!!</h1>            
+        <div className="container">
+            <h1>Carts component!</h1>
+            <div>
+                <h2>Todos os Carrinhos do aplicativo</h2>
+            </div>
+            <div>
+                <p>Consulte abaixo todos os carrinhos nas opções para selecionar os filtros.</p>
+
+            </div>
+            <div>
+                <Link to="/">
+                    <button>Voltar</button>
+                </Link>
+                <Link to="/carts">
+                <button>Criar Cart</button>
+                </Link>
+                <Link to="/products">
+                    <button>Buscar Cart</button>
+                </Link>
+                <div><p>Query aqui: <input></input></p>
+                </div>
+            </div>
+            <hr></hr>
+            <div className="tabela">
+                <table>
+                    <tr>
+                        <th>Numero</th>
+                        <th>Usuario</th>
+                        <th>Qtde Itens</th>
+                        <th>Valor</th>
+                    </tr>
+
+                    <tr>
+                        <td>CART00001</td>
+                        <td>Maria Anders</td>
+                        <td>5</td>
+                        <td>R$1234</td>
+                    </tr>
+                    <tr>
+                        <td>CART00002</td>
+                        <td>Francisco Chang</td>
+                        <td>2</td>
+                        <td>R$355</td>
+                    </tr>
+                    <tr>
+                        <td>CART00003</td>
+                        <td>Antonio</td>
+                        <td>6</td>
+                        <td>R$7500</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     )
 }
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+import './home.css'
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home component!</h1>            
+        <div className="container">
+            <h1>Home component!</h1>
+            <div>
+                <h2>Seja bem vindo à home page do seu aplicativo</h2>
+            </div>
+            <div>
+                <p>Consulte abaixo clique nas opções para selecionar os filtros.</p>
+
+            </div>
+            <div>
+                <a href="/products"><button>Produtos</button></a>
+                <a href="/carts"><button>Carrinhos</button></a>
+                <a href="/users"><button>Usuarios</button></a>
+            </div>
         </div>
     )
 }
